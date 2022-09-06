77288c4665f87debd236537a4493f9dd47ff6eac
Siripoom <siripoom.kusonsong@kmutt.mail.ac.th>
2022-09-06 09:28:17 +0700

    code : complete

--- minMedMax.js
+++ minMedMax.js
@@ -1,6 +1,17 @@
-const { template } = require('@babel/core')
+const { template } = require("@babel/core");
 
 function minMedMax(n1, n2, n3) {
-  //code here
+  let arr = [n1, n2, n3];
+  let temp;
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[i] > arr[j]) {
+        temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+      }
+    }
+  }
+  return { min: arr[0], mid: arr[1], max: arr[2] };
 }
-module.exports = minMedMax
+module.exports = minMedMax;
